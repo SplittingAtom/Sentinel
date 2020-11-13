9686199d3768e10f84050529931f21aaf1687bf5
cpv0310 <cpv0310@live.com>
2020-11-13 01:08:25 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ docker pull chronograf
 docker-compose up -d
 sudo chown -R 472:472 /grafana/data
 
-
-curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE '
+#Show Databases in Influx
+curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
