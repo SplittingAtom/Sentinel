817181b3a43487efaf4761bf0746b3b264440dc2
cpv0310 <cpv0310@live.com>
2020-11-13 22:32:38 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 docker pull grafana/grafana
-docker pull influxdb:1.8.3
+docker pull influxdb
 docker pull telegraf
 docker pull chronograf
 
 docker-compose up -d
-sudo chown -R 472:472 /grafana/data
+sudo chown -R 472:472 grafana/data
 
 #Show Databases in Influx
 curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
